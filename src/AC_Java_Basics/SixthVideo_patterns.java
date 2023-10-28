58538a06d471f909dda09bc0800c226ccc1ff349
swayam-12205790 <swayamswarupbarik25@gmail.com>
2023-10-28 19:12:06 +0530

    java basics started

--- src/AC_Java_Basics/SixthVideo_patterns.java
+++ src/AC_Java_Basics/SixthVideo_patterns.java
@@ -128,7 +128,6 @@ public class SixthVideo_patterns {
 //        1 0 1
 //        0 1 0 1
 //        1 0 1 0 1
-
 //        int n=5;
 //        for (int i = 1; i <= n ; i++) {
 //            for (int j = 1; j <= i ; j++) {
