d7ad4869e0f0bf179e33d5159a0bafbe401a2da9
swayam-12205790 <swayamswarupbarik25@gmail.com>
2023-10-29 10:56:30 +0530

    java basics started

---
+++ src/AC_Java_Basics/EigthVideo_Functions.java
@@ -0,0 +1,86 @@
+package AC_Java_Basics;
+
+//public class EigthVideo_Functions {
+//        public static void main(String[] args) {
+//    //function name cannot be keyword
+//        Scanner inp = new Scanner(System.in);
+//        int num1 = inp.nextInt();
+//        int num2 = inp.nextInt();
+//        int mul=printMyMultiplication(num1,num2);
+//        System.out.println(mul);
+//    }
+//    public static int printMyMultiplication(int num1, int num2){
+//        int mul = num1*num2;
+//        return mul;
+//    }
+//// functions are called directly while methods are called with the help of objects of a class
+//}
+
+//Q: enter 3 numbers fro  the user & make a function to print their average
+//public class EigthVideo_Functions{
+//    public static void main(String[] args) {
+//        Scanner inp = new Scanner(System.in);
+//        int num1 = inp.nextInt();
+//        int num2 = inp.nextInt();
+//        int num3 = inp.nextInt();
+//        int average = Average(num1,num2,num3);
+//        System.out.println(average);
+//    }
+//
+//    public static int Average(int num1,int num2,int num3) {
+//        int average = (num1 +num2+num3)/3;
+//        return average;
+//    }
+//}
+
+//Q:write a function to print the sum of all odd numbers from 1 to n;
+//public class EigthVideo_Functions{
+//    public static void main(String[] args) {
+//        Scanner inp = new Scanner(System.in);
+//        int n = inp.nextInt();
+//        System.out.println(printMySum(n));
+//    }
+//
+//    public static int printMySum(int number) {
+//        int sum =0;
+//        for (int i = 1; i <=number; i++) {
+//            if (i%2 != 0){
+//                sum = sum+i;
+//            }
+//        }
+//        return sum;
+//    }
+//}
+
+//Q:write a function which takes in 2 numbers and returns the greater of those 2
+//public class EigthVideo_Functions{
+//    public static void main(String[] args) {
+//        Scanner inp = new Scanner(System.in);
+//        int n = inp.nextInt();
+//        int n2 = inp.nextInt();
+//        System.out.println(greaterOfTwo(n,n2));
+//    }
+//    public static int greaterOfTwo(int number,int number2) {
+//        if(number>number2){
+//            return number;
+//        }
+//        return number2;
+//    }
+//}
+
+//Q;write a function that takes in the radius as input and returns the circumference of a circle
+//public class EigthVideo_Functions{
+//    public static void main(String[] args) {
+//        Scanner inp = new Scanner(System.in);
+//        int n = inp.nextInt();
+//        System.out.println(circumOfCircle(n));
+//    }
+//
+//    public static double circumOfCircle(int radius) {
+//        double circum = 2* Math.PI*radius;
+//        return circum;
+//
+//    }
+//}
+
+
---
+++ src/AC_Java_Basics/OOPS.java
@@ -0,0 +1,60 @@
+package AC_Java_Basics;
+class student {
+    String name;
+    int age;
+    int rollno;
+    String courseStudy;
+    String Hostel;
+
+    public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.rollno);
+        System.out.println(this.courseStudy);
+        System.out.println(this.Hostel);
+    }
+
+    //   student()  //non-parameterised constructor
+//   {
+//       System.out.println("here Constructor is called");
+//   }
+//}
+//public class OOPS {
+//    public static void main(String[] args) {
+//        student s1 = new student(); //constructors to construct objects same name as class and no return type and called only once
+//        s1.name="swayam";
+//        s1.age=20;
+//        s1.rollno=22;
+//        s1.courseStudy="B.tech C.S.E";
+//        s1.Hostel = "Bh1";
+//
+//        s1.printInfo();
+//    }
+//}
+//    student(String name,int age){ //parameterised constructor
+//        this.name=name;
+//        this.age=age;
+//    }
+//}
+//    public class OOPS {
+//        public static void main(String[] args) {
+//            student s1 = new student("Swayam", 20); //constructors to construct objects same name as class and no return type and called only once
+//
+//            s1.printInfo();
+//        }
+//    }
+
+    public class OOPS {
+        public static void main(String[] args) {
+            student s1 = new student(); //constructors to construct objects same name as class and no return type and called only once
+            s1.name = "swayam";
+            s1.age = 20;
+            s1.rollno = 22;
+            s1.courseStudy = "B.tech C.S.E";
+            s1.Hostel = "Bh1";
+
+            s1.printInfo();
+        }
+    }
+}
+
