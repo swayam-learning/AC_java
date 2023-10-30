4f23d580b8f1714f5b0f9b89b947eb1c0df54eda
swayam-12205790 <swayamswarupbarik25@gmail.com>
2023-10-30 16:37:42 +0530

    java basics started

---
+++ src/AC_Java_Basics/kk_functions/Fucntions_methods.java
@@ -0,0 +1,4 @@
+package AC_Java_Basics.kk_functions;
+
+public class Fucntions_methods {
+}
---
+++ src/AC_Java_Basics/polymorphism.java
@@ -0,0 +1,24 @@
+package AC_Java_Basics;
+
+class vidyarthi{
+    String name;
+    int age;
+    public void printinfo(String name)
+    {
+        this.name = name;
+        System.out.println("name of the student is "+name);
+    }
+    public void printinfo(int age){
+        System.out.println(age);
+    }
+}
+public class polymorphism {
+    public static void main(String[] args) {
+        vidyarthi s1=new vidyarthi();
+        s1.age = 20;
+        s1.printinfo("swayam");
+        s1.printinfo(s1.age);
+    }
+}
+//this is called function overloading that is the functions can have same name but the no of parameters should be different,\
+// if the number of parameter is same then there return type should be different
