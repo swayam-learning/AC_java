0d30ea77482b922e4bd0cd29e643d1d3e72c2bc9
swayam-12205790 <swayamswarupbarik25@gmail.com>
2023-11-01 18:06:34 +0530

    java basics started

--- src/AC_Java_Basics/kk_functions/Functions_methods.java
+++ src/AC_Java_Basics/kk_functions/Functions_methods.java
@@ -1,13 +1,5 @@
 package AC_Java_Basics.kk_functions;
-class vidyaarth{
-    int[] rollno = new int[5];
-    String[] name = new String[5];
-    float[] marks = new float[5];
-    // class is a logical construct
-    //object is an instance of a class
-    //state of the object is basically the value of the object
 
-}
 public class Functions_methods {
     public static void main(String[] args) {
         System.out.println("Just checking");
